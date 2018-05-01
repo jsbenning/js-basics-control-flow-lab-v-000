cdcabc98a553a68d53ae2e943fb062ddc7ccf789
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-01 23:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,9 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     case ("generous"): 
       return "Thank you so much.";
-      break;
     case ("not as generous"): 
       return "Thank you.";
-      break;
     default:
       return "Bye.";
-      break;
   }  
 }
