1be782055c83faaf0780ff7ad427ff6895bf1f45
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-01 22:45:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400){
     return "This one is on me!";
-  } else if 
+  } else if (distance >= 2000 && < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500)
   
 }
